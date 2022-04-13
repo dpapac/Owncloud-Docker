25b56612e040b0fee3079ea54508f2a6e476b9d6
dpapac <dpapac@owncloud.com>
2022-04-13 20:37:21 +0200

    Setup example Theme in Owncloud and enable it

---
+++ buildFilesDocker/themeSetupDocker.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#Install unzip
+sudo apt install unzip
+
+#Change into repositroy the example will be stored in 
+cd /var/www/owncloud/apps-external
+
+#Download the theme 
+wget https://github.com/owncloud/theme-example/archive/master.zip
+
+#Extract 
+unzip master.zip
+
+#Rename theme
+mv theme-example-master dockerThemes
+
+#Rename it in /dockerThemes/appinfo/info.xml
+sed -i "s#<id>theme-example<#<id>dockerThemes<#" "dockerThemes/appinfo/info.xml"
+
+#Adjust the permissions
+sudo chown -R www-data: dockerThemes
+
+#Enable Themes
+occ app:enable dockerThemes
+
+#'integrity.ignore.missing.app.signature' => [
+#       'mynewtheme',
+#],
+
