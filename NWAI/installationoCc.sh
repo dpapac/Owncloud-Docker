6f5be0fe65c322728e7a74d6019e236aabef4403
dpapac <dpapac@owncloud.com>
2022-04-13 12:02:52 +0200

    Dockerfile and installation.sh/startup.sh script added

---
+++ NWAI/installationoCc.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+apt update && \
+  apt upgrade -y
+
+#Create the occ Helper Script
+
+FILE="/usr/local/bin/occ"
+cat <<EOM >$FILE
+#! /bin/bash
+cd /var/www/owncloud
+sudo -E -u www-data /usr/bin/php /var/www/owncloud/occ "\$@"
+EOM
+chmod +x $FILE
+
+#Install required packages
+
+apt install -y \
+  apache2 \
+  libapache2-mod-php \
+  mariadb-server \
+  openssl redis-server wget \
+  php-imagick php-common php-curl \
+  php-gd php-imap php-intl \
+  php-json php-mbstring php-mysql \
+  php-ssh2 php-xml php-zip \
+  php-apcu php-redis php-ldap \
+  php-opcache
+  
+#Install recommmended Packages
+
+apt install -y \
+  unzip bzip2 rsync curl jq \
+  inetutils-ping  ldap-utils\
+  smbclient
+  
+#Configure Apache and create a virtual Host Configuration
+
+FILE="/etc/apache2/sites-available/owncloud.conf"
+cat <<EOM >$FILE
+<VirtualHost *:80>
+# ServerName <add a valid ServerName> and uncommment
+DirectoryIndex index.php index.html
+DocumentRoot /var/www/owncloud
+<Directory /var/www/owncloud>
+  Options +FollowSymlinks -Indexes
+  AllowOverride All
+  Require all granted
+
+ <IfModule mod_dav.c>
+  Dav off
+ </IfModule>
+
+ SetEnv HOME /var/www/owncloud
+ SetEnv HTTP_HOME /var/www/owncloud
+</Directory>
+</VirtualHost>
+EOM
+
+#Enable the Configuration
+
+a2dissite 000-default
+a2ensite owncloud.conf
+
+#Enable recommended Apache Modules
+
+echo "Enabling Apache Modules"
+a2enmod dir env headers mime rewrite setenvif
+
+#Download latest Version of ownCloud
+
+cd /var/www/
+wget -q https://download.owncloud.org/community/owncloud-complete-latest.tar.bz2 && \
+tar -xjf owncloud-complete-latest.tar.bz2 && \
+chown -R www-data. owncloud
+
+#Sets Up a Cron Job
+
+echo "*/15  *  *  *  * /var/www/owncloud/occ system:cron" \
+  | sudo -u www-data -g crontab tee -a \
+  /var/spool/cron/crontabs/www-data
+
+#Configure Log Rotation
+
+FILE="/etc/logrotate.d/owncloud"
+sudo cat <<EOM >$FILE
+/var/www/owncloud/data/owncloud.log {
+  size 10M
+  rotate 12
+  copytruncate
+  missingok
+  compress
+  compresscmd /bin/gzip
+}
+EOM
+
+#Ensure that permissions are correct
+
+cd /var/www/
+chown -R www-data. owncloud
---
+++ NWAI/startup.sh
@@ -0,0 +1,51 @@
+#Start the database
+/etc/init.d/mysql restart
+sleep 5
+#Configure the Database
+mysql <<EOF
+ CREATE USER IF NOT EXISTS 'dbadmin'@'localhost' IDENTIFIED BY 'password';
+ GRANT ALL PRIVILEGES ON *.* TO 'dbadmin'@'localhost' WITH GRANT OPTION;
+ FLUSH PRIVILEGES;
+ SHOW GRANTS FOR 'dbadmin'@'localhost';
+ exit
+EOF
+
+#For occ maintenance
+/etc/init.d/redis-server restart
+/etc/init.d/apache2 restart
+sleep 5
+#Install ownCloud
+
+occ maintenance:install \
+    --database "mysql" \
+    --database-name "owncloud" \
+    --database-user "dbadmin" \
+    --database-pass "password" \
+    --data-dir "/var/www/owncloud/data" \
+    --admin-user "admin" \
+    --admin-pass "admin"
+
+#Configure ownCloud's Trusted Domains
+
+myip=$(hostname -I|cut -f1 -d ' ')
+occ config:system:set trusted_domains 1 --value="$myip"
+
+#Set background job mode to cron:
+
+occ background:cron
+
+#Configure Caching and File Locking
+
+occ config:system:set \
+   memcache.local \
+   --value '\OC\Memcache\APCu'
+occ config:system:set \
+   memcache.locking \
+   --value '\OC\Memcache\Redis'
+occ config:system:set \
+   redis \
+   --value '{"host": "127.0.0.1", "port": "6379"}' \
+   --type json
+
+echo "The installation was finished, your current installed Version is:"
+occ status
